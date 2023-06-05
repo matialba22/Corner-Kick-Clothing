74e722afbea6c1b24399a9f90f175bd688ba0a1d
matialba22 <albarracinmatias9@gmail.com>
2023-06-05 15:52:31 -0300

    Shop routes and controller updated

---
+++ src/controllers/shopController.js
@@ -0,0 +1,7 @@
+const shopController = {
+  shop: (req, res) => {
+    res.render("./products/shop");
+  },
+};
+
+module.exports = shopController;
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const shopController = require("../controllers/shopController");
+
+router.get("/", shopController.shop);
 
 module.exports = router;
