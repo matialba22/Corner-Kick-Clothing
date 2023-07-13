94edd1162003bfcebc6ed0b6396e41068e6f05ab
matialba22 <albarracinmatias9@gmail.com>
2023-07-13 16:04:00 -0300

    Installed express-session, bcrypt and cookieParser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
+const session = require("express-session");
+const a = "";
 
 // ************ Template engine ************
 app.set("view engine", "ejs");
@@ -12,6 +14,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.use(session({ secret: "This is secret!!" }));
+app.use(cookieParser());
 
 // ************ Server ************
 app.listen(process.env.PORT || 3030, () => "Server running on port 3030");
@@ -20,6 +24,7 @@ app.listen(process.env.PORT || 3030, () => "Server running on port 3030");
 const mainRoutes = require("./src/routes/main");
 const shopRoutes = require("./src/routes/shop");
 const usersRoutes = require("./src/routes/users");
+const cookieParser = require("cookie-parser");
 
 app.use("/", mainRoutes);
 app.use("/shop", shopRoutes);
