521f74193bf79f6842ad6b3069a9bd27b74f6daf
matialba22 <albarracinmatias9@gmail.com>
2023-10-05 17:39:51 -0300

    Prod page styles

--- public/js/prodPage.js
+++ public/js/prodPage.js
@@ -2,8 +2,9 @@ window.addEventListener("load", () => {
 
     const mainImage = document.querySelector("div.mainImage img");
     const miniImages = document.querySelectorAll("div.miniImage img");
+    const dropDowns = document.querySelectorAll("div.btn-drop");
 
-    let selectedImageIndex = 0
+    let selectedImageIndex = 0;
 
     miniImages.forEach((miniImg, i) => {
 
@@ -37,5 +38,17 @@ window.addEventListener("load", () => {
         });
 
     });
+
+    dropDowns.forEach((dropDown) => {
+      dropDown.addEventListener("click", () => {
+        const dropContent = dropDown.nextElementSibling;
+        const icon = dropDown.querySelector(".fa-chevron-down");
+
+        dropContent.classList.toggle("open");
+
+        icon.classList.toggle("rotated");
+      
+      })
+    })
 
 });
