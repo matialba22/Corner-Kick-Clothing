6875e0b44baa53f6848e53ddf02d2f8c982cf07e
matialba22 <albarracinmatias9@gmail.com>
2023-10-30 13:11:47 -0300

    Added JQuery for slick + carousel

---
+++ public/js/slickCarrousel.js
@@ -0,0 +1,9 @@
+$('.slick-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+  nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+  dots: false,
+});
